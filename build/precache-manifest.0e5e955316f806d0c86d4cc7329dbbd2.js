77f511c3890a023612327ef396ee809a2425ccb1
puzansakya <puzansakya@gmail.com>
2020-06-03 18:19:53 +0545

    updated build

---
+++ build/precache-manifest.0e5e955316f806d0c86d4cc7329dbbd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f50c63e68e7d1636de6a3c54a061bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d8bf4546135ee48617",
+    "url": "/static/css/main.56d90b7a.chunk.css"
+  },
+  {
+    "revision": "71732f5115fa1136352e",
+    "url": "/static/js/2.c07348ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c07348ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d8bf4546135ee48617",
+    "url": "/static/js/main.24e5ef41.chunk.js"
+  },
+  {
+    "revision": "4b43dc8aaf4c3449b36a",
+    "url": "/static/js/runtime-main.ef501f69.js"
+  }
+]);
